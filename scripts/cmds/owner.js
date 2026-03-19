7596edab7b5d3ba2c01ca741a31071a22835021d
mdsiyam741147-dot <mdsiyam741147@gmail.com>
2026-03-20 01:51:32 +0600

    Update owner.js

--- scripts/cmds/owner.js
+++ scripts/cmds/owner.js
@@ -113,15 +113,15 @@ ctx.textAlign = "left";
 ctx.font = `bold ${16 * scale}px Arial`; 
 
 // Card Content
-ctx.fillText(`🤖 Bot Name: [ MILON BOT ]`, infoX, nameY + (25 * scale)); 
+ctx.fillText(`🤖 Bot Name: [ Siyam BOT ]`, infoX, nameY + (25 * scale)); 
 ctx.fillText(`⚙️ Prefix: [ ${threadPrefix} ]`, infoX, nameY + (45 * scale)); 
 ctx.fillText(`⏳ Uptime: [ ${uptimeString} ]`, infoX, nameY + (65 * scale)); 
 ctx.fillText(`📊 Commands: [ ${totalCommands} ]`, infoX, nameY + (85 * scale)); 
-ctx.fillText(`👤 Owner: [ Milon Hasan ]`, infoX, nameY + (115 * scale)); 
-ctx.fillText(`📅 Age: [ 17+ ]`, infoX, nameY + (135 * scale)); 
-ctx.fillText(`📍 Address: [ NARAYANGANJ ]`, infoX, nameY + (155 * scale)); 
+ctx.fillText(`👤 Owner: [ 𝕌𝕕𝕒𝕪 ℍ𝕒𝕤𝕒𝕟 𝕊𝕚𝕪𝕒𝕞]`, infoX, nameY + (115 * scale)); 
+ctx.fillText(`📅 Age: [ 16+ ]`, infoX, nameY + (135 * scale)); 
+ctx.fillText(`📍 Address: [ Kishoreganj ]`, infoX, nameY + (155 * scale)); 
 ctx.fillText(`📝 Status: [ silence is my attitude ]`, infoX, nameY + (175 * scale)); 
-ctx.fillText(`📞 WhatsApp: [ 880 1912603270 ]`, infoX, nameY + (195 * scale)); 
+ctx.fillText(`📞 WhatsApp: [ 880 1789138157 ]`, infoX, nameY + (195 * scale)); 
 
 const cacheDir = path.join(__dirname, "cache");
 fs.ensureDirSync(cacheDir);
@@ -134,13 +134,13 @@ const caption =
 "╔══════════════════╗\n" +
 " ✨ 𝗕𝗢𝗧 & 𝗢𝗪𝗡𝗘𝗥 𝗜𝗡𝗙𝗢 ✨\n" +
 "╚══════════════════╝\n\n" +
-"👤 𝗢𝘄𝗻𝗲𝗿: Milon Hasan\n" +
-"🤖 𝗕𝗼𝘁 𝗡𝗮𝗺𝗲: MILON BOT\n" +
+"👤 𝗢𝘄𝗻𝗲𝗿: 𝕌𝕕𝕒𝕪 ℍ𝕒𝕤𝕒𝕟 𝕊𝕚𝕪𝕒𝕞\n" +
+"🤖 𝗕𝗼𝘁 𝗡𝗮𝗺𝗲: siyam BOT\n" +
 "⚙️ 𝗣𝗿𝗲𝗳𝗶𝘅: [ " + threadPrefix + " ]\n" +
 "⏳ 𝗨𝗽𝘁𝗶𝗺𝗲: " + uptimeString + "\n" +
 "📊 𝗖𝗼𝗺𝗺𝗮𝗻𝗱𝘀: " + totalCommands + "\n" +
-"📞 𝗪𝗵𝗮𝘁𝘀𝗔𝗽𝗽: +880 1912603270\n" +
-"📍 𝗔𝗱𝗱𝗿𝗲𝘀𝘀: Narayanganj, Bangladesh\n" +
+"📞 𝗪𝗵𝗮𝘁𝘀𝗔𝗽𝗽: +880 1789138157\n" +
+"📍 𝗔𝗱𝗱𝗿𝗲𝘀𝘀: Kishoreganj , Bangladesh\n" +
 "━━━━━━━━━━━━━━━━━━━━\n" +
 "✅ 𝗣𝗿𝗲𝗺𝗶𝘂𝗺 𝗖𝗮𝗿𝗱 𝗗𝗲𝗹𝗶𝘃𝗲𝗿𝗲𝗱!\n" +
 "━━━━━━━━━━━━━━━━━━━━";
